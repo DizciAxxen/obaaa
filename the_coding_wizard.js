97d03c885a694abc7d6fb3bc4f4173fc4eaf8fdf
DizciAxxen <Axxen41@gmail.com>
2025-04-24 12:22:22 +0200

    Update the_coding_wizard.js

--- the_coding_wizard.js
+++ the_coding_wizard.js
@@ -1,33 +1,24 @@
 const element = document.getElementById("element");
-let mouse = { x: 0, y: 0 };
-const fps = 60;
-const delay = 10;
-const parts = [{ z: 1 }, { z: 2 }, { z: 3 }, { z: 4 }]; // Örnek parça yapısı
 
-function init() {
-  document.addEventListener("mousemove", mousemove);
-  setInterval(loop, 1000 / fps);
-}
+let posX = -200;
+let direction = 1;
 
-function mousemove(e) {
-  mouse = { x: e.clientX, y: e.clientY };
+function svg(x) {
+  return `
+    <svg viewBox="0 0 500 300" style="left: ${x}px; top: 150px;" xmlns="http://www.w3.org/2000/svg">
+      <path d="M50,150 Q150,100 250,150 T450,150 Q400,180 350,150 T50,150 Z"
+            fill="#7FDBFF" stroke="#0074D9" stroke-width="4"/>
+      <circle cx="80" cy="140" r="5" fill="#111"/>
+      <path d="M100,150 Q110,140 120,150" stroke="#001f3f" stroke-width="2" fill="none"/>
+    </svg>
+  `;
 }
 
 function loop() {
-  for (var i = 0; i < parts.length; i++) {
-    var params = { mouse: mouse, part: parts[i] };
-    setTimeout(transform, parts[i].z * delay, params);
-  }
-  element.innerHTML = svg();
+  posX += direction * 2;
+  if (posX > window.innerWidth) posX = -200;
+  element.innerHTML = svg(posX);
+  requestAnimationFrame(loop);
 }
 
-function transform(params) {
-  // Burada parçaların konumlarını değiştirirsin
-}
-
-function svg() {
-  // Burada SVG görselini oluşturursun
-  return `<svg width="300" height="300"><text x="10" y="50">Shark SVG Buraya</text></svg>`;
-}
-
-init();
+loop();
